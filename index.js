7e455d4a045b7b88592bd7afd54e89e8f3008932
KarenGiullia <138712215+KarenGiullia@users.noreply.github.com>
2025-07-28 14:55:49 -0300

    Commit 6 - Fatura em HTML

--- index.js
+++ index.js
@@ -74,8 +74,30 @@ function gerarFaturaStr(fatura, pecas) {
   return faturaStr;
 }
 
+function gerarFaturaHTML(fatura, pecas) {
+  let html = `<html>\n`;
+  html += `<p> Fatura ${fatura.cliente} </p>\n`;
+  html += `<ul>\n`;
+
+  for (let apre of fatura.apresentacoes) {
+    const total = calcularTotalApresentacao(pecas, apre);
+    html += `<li> ${getPeca(pecas, apre).nome}: ${formatarMoeda(total)} (${apre.audiencia} assentos) </li>\n`;
+  }
+
+  html += `</ul>\n`;
+  html += `<p> Valor total: ${formatarMoeda(calcularTotalFatura(pecas, fatura.apresentacoes))} </p>\n`;
+  html += `<p> Créditos acumulados: ${calcularTotalCreditos(pecas, fatura.apresentacoes)} </p>\n`;
+  html += `</html>`;
+
+  return html;
+}
+
 const faturas = JSON.parse(readFileSync('./faturas.json'));
 const pecas = JSON.parse(readFileSync('./pecas.json'));
 
-const faturaStr = gerarFaturaStr(faturas, pecas);
-console.log(faturaStr);
+const faturaTexto = gerarFaturaStr(faturas, pecas);
+const faturaHtml = gerarFaturaHTML(faturas, pecas);
+
+console.log(faturaTexto);
+console.log('\n-----\n');
+console.log(faturaHtml);
